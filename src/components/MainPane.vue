4b4775d9504c9394d2b8f3ce9b3f537d59fc55ec
Huy-DNA <huydo862003@gmail.com>
2025-05-21 19:28:17 +0700

    feat: plugin compiler

--- src/components/MainPane.vue
+++ src/components/MainPane.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import type { Error } from '@/core/mips/types';
 import Editor from './editor/Editor.vue';
+
+const emit = defineEmits<{
+  (e: 'errorsChange', errors: Error[]): void,
+}>();
 </script>
 
 <template>
-  <Editor />
+  <Editor @errors-change="(errors) => emit('errorsChange', errors)" />
 </template>
--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -3,10 +3,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch, type Ref } from 'vue';
 import * as monaco from 'monaco-editor';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js?worker&inline';
 import { initialCode } from './initialCode.ts';
+import { lex } from '@/core/mips/lex.ts';
+import { parse } from '@/core/mips/parse.ts';
+import { validate } from '@/core/mips/validate.ts';
+import type { Error } from '@/core/mips/types.ts';
+
+const emit = defineEmits<{
+  (e: 'errorsChange', errors: Error[]): void,
+}>();
 
 const editorWorker = new EditorWorker();
 
@@ -17,12 +25,32 @@ const editorWorker = new EditorWorker();
 };
 
 const editorRef = ref(null);
+let model: monaco.editor.IModel;
+let uri: monaco.Uri;
+const source = ref(initialCode);
+const errors: Ref<Error[]> = ref([]);
 
 onMounted(() => {
+  uri = new monaco.Uri();
+  model = monaco.editor.createModel(initialCode, 'mips', uri);
+
   monaco.editor.create(editorRef.value, {
-    language: 'dbml',
+    language: 'mips',
     value: initialCode,
     automaticLayout: true,
+    model: model,
   });
+
+  model.onDidChangeContent(() => {
+    source.value = model.getValue();
+  })
+});
+
+watch(source, () => {
+  const tokenResult = lex(source.value);
+  const nodeResult = parse(source.value, tokenResult.result);
+  const validateResult = validate(source.value, tokenResult.result, nodeResult.result);
+  errors.value = [...tokenResult.errors, ...nodeResult.errors, ...validateResult.errors];
+  emit('errorsChange', errors.value);
 });
 </script>
